d91dd87873ed8bc6babdf3b1bbfef0d3a5735612
Vale <vali.vogt@gmx.de>
2020-07-25 00:55:10 +0200

    cufuse

--- website/js/Index.js
+++ website/js/Index.js
@@ -124,14 +124,14 @@ var Eisdiele;
                     let button = document.createElement("button");
                     button.setAttribute("class", "creationButton");
                     button.addEventListener("click", onClickCreate.bind(jsonObj[index]));
-                    if (jsonObj[index].kategorie == "Eis")
+                    if (jsonObj[index].kategorie != "Eis")
                         formatDiv.appendChild(button).innerHTML = "Ab in die Kreation! ";
                 }
                 if (isTopping) {
                     let button = document.createElement("button");
                     button.setAttribute("class", "creationButton");
                     button.addEventListener("click", onClickCreate.bind(jsonObj[index]));
-                    if (jsonObj[index].kategorie != "Eis")
+                    if (jsonObj[index].kategorie != "Eis" && "Waffel")
                         formatDiv.appendChild(button).innerHTML = "Ab in die Kreation! ";
                 }
             }
